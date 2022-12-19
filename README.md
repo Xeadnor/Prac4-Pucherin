60b983be96c84b8d47188dabfe6911825758e20e
Marta <m.torresdlc@gmail.com>
2022-12-19 20:53:22 +0100

    modificación de función jugar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Prac4-Pucherin
+# Práctica 4- El juego del Pucherín
 
 
 https://xeadnor.github.io/Prac4-Pucherin/
--- js/jugar.js
+++ js/jugar.js
@@ -19,19 +19,22 @@ function grafico(e) {
 
     };
   }
+
+  //array de jugadores que se rellena según el num selecionado
   const array = [];
   for (let i = 1; i <= jugadores.value; i++) {
-    let jugador = crearJugador("Jugador" + i);
+    let jugador = crearJugador("Jugador " + i);
     array.push(jugador);
   }
 
-if(valorFicha[0].checked){//comprbacion visa
+if(valorFicha[0].checked){//comprbacion visa ????????????????????????????????????????????
   fichasTotales = 30;
 }else if (valorFicha[1].checked) {
   fichasTotales = 40;
 }else if (valorFicha[2].checked) {
   fichasTotales = 50;
 }
+
   document.getElementById("fichasR").innerHTML = "Quedan por dar " + fichasTotales + " fichas";
   document.getElementById("opciones").style.display="none" 
   document.getElementById("grafico").style.display="block" 
@@ -40,6 +43,7 @@ if(valorFicha[0].checked){//comprbacion visa
     text += ("<div class='row justify-content-center'><div class='col-3 border'>" + array[i].name  + "</div><div class='col-3 border'>Fichas:X</div> </div>")    + "<br>";
     
   }
+
   document.getElementById("listajugadores").innerHTML= text;
 
   //Cambiar boton a tirar dados:
@@ -48,39 +52,59 @@ if(valorFicha[0].checked){//comprbacion visa
     botonJ.innerHTML="Tirar dados"
   }
 
-
-
   //Cambia título del encabezado
   function cambiarTurnoJugador(jugador) {
     turno.innerHTML= "Turno del jugador: " + jugador;
   }
 
-
-
-
-  //Función principal del juego que llama al resto de funciones
-  function Jugar(e) {
-    e.preventDefault();
-    if(botonJ.innerHTML == "Tirar dados"){
-      var numeroDados = tirarDados();
-      nDados.innerHTML = numeroDados
-
-      var primerT = primerTurno();
-      cambiarTurnoJugador(array[primerT-1].name)
-    }
-    cambiarTextoBoton();
-    
-  }
-
+  
+  //función que con un random saca la tirada de los dados
   function tirarDados(){
     return Math.floor(Math.random() * (12 - 2 + 1) + 2)
   }
 
+  //función que decide qué jugador empieza a jugar
   function primerTurno(){
-    console.log(Math.floor(Math.random() * ((jugadores.value-1) - 0 + 1) + 0))
-
-    return Math.floor(Math.random() * (jugadores.value - 2 + 1) + 2)
-    
+        return Math.floor(Math.random() * (jugadores.value - 1 + 1) + 1)
   }
+
+
+
+
+
+
+  //---------------Función principal del juego que llama al resto de funciones-------------
+  function Jugar(e) {
+    e.preventDefault();
+
+    if(botonJ.innerHTML == "Tirar dados"){
+      var primerT = primerTurno();
+      cambiarTurnoJugador(array[primerT-1].name)
+
+      var numeroDados = tirarDados();
+      nDados.innerHTML = numeroDados
+
+      //pintar casilla de la ficha correspondiente al num del dado
+      if(numeroDados!=7 && numeroDados!=12 ){
+        pintarCasilla(canvases[numeroDados-2],numeroDados, 1);
+      }else{
+        //TODO aquí falta añadir lo que haría en caso de sacar 7
+      }//TODO else lo que haría al sacar 12, o tambien se puede hacer con un switch
+      
+
+      //restar una ficha al total de fichas del jugador
+      array[primerT-1].fichasRecibidas-=1;
+
+      //TODO aquí iría el cambio de turno
+        /* if (primerT != array.lenght ){
+          primerT++
+        }else{
+          primerT=1
+        }  y se volvería a empezar lo anterior */
+
+    }
+    cambiarTextoBoton();
+  }
+
 
 }
